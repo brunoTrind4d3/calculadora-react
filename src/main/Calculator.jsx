3f40bc78013f31b91b2ef0ba287b7226e803ba67
brunoTrind4d3 <bruno.eng.ufg@gmail.com>
2019-12-08 18:05:30 -0300

    Iniciando projeto

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,51 @@
+import React , {Component} from 'react'
+import './Calculator.css'
+import Button from '../components/Button'
+import Display from '../components/Display'
+
+
+
+export default class Calculator extends Component{
+
+    clearMemory(){
+        console.log('Limpar')
+    }
+
+    setOperation(operation){
+        console.log(operation)
+    }
+
+    addDigit(n){
+        console.log(n)
+    }
+
+    render(){
+
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
+
+        return(
+            <div className='calculator'>
+                <Display value={0}/>
+                <Button label='AC' click={()=> this.clearMemory()} triple/>
+                <Button label='/' click={setOperation} operation/>
+                <Button label='7' click={addDigit}/>
+                <Button label='8' click={addDigit}/>             
+                <Button label='9' click={addDigit}/>
+                <Button label='*' click={setOperation} operation/>
+                <Button label='4' click={addDigit}/>
+                <Button label='5' click={addDigit}/>
+                <Button label='6' click={addDigit}/>
+                <Button label='-' click={setOperation} operation/>
+                <Button label='1' click={addDigit}/>
+                <Button label='2' click={addDigit}/>
+                <Button label='3' click={addDigit}/>
+                <Button label='+' click={setOperation} operation/>
+                <Button label='0' click={addDigit} double/>
+                <Button label='.' click={addDigit}/>
+                <Button label='=' click={setOperation} operation/>
+            </div>
+        )
+    }
+    
+}
